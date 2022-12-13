27ffa99b990dcb0bd89b800c67b79d8db040e574
Oyin11 <realoyins@yahoo.com>
2022-12-12 22:49:20 -0800

    Tasks2to4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef _LINKED_LIST
 #define _LINKED_LIST
-#include <stdlib.h>
 /**
 * struct list_s - singly linked list
 *@str: string - (malloc'ed string)
@@ -16,6 +15,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
